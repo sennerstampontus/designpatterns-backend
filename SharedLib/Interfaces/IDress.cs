a270d0104d2b09546a52d55c9a5ffd7078ed41ff
Pontus Sennerstam <pontus.sennerstam@learnet.se>
2022-12-16 15:15:45 +0100

    :fire: removed previous projects

--- SharedLib/Interfaces/IDress.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace designpatterns_api.Interfaces
-{
-    public interface IDress : IProduct
-    {
-        public string DressLength { get; set; }
-        public string Sleeves { get; set; }
-    }
-}
